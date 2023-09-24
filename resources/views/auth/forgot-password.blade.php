cdf52a474565bd30dd3a572b994e1b707b9efd00
Kim Lanisa <lanisriagustin137@gmail.com>
2023-09-24 21:17:13 +0700

    redesign forgot password pages 🔑

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,34 +1,128 @@
-<x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+<!DOCTYPE html>
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+<html
+  lang="en"
+  class="light-style customizer-hide"
+  dir="ltr"
+  data-theme="theme-default"
+  data-assets-path="../../assets/"
+  data-template="vertical-menu-template"
+>
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
+    />
+
+    <title>Forgot Password | To-Do Class</title>
+
+    <meta name="description" content="To-Do Class is a simple app to manage your daily tasks with a simple user interface." />
+    <meta name="keywords" content="To-Do Class, To-Do, To-Do List, To-Do App, To-Do List App, To-Do List Application" />
+    <meta name="author" content="Kim & Rey" />
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="../../assets/img/favicon/favicon.ico" />
+
+    @include('includes.backend.style')
+  </head>
+
+  <body>
+    <!-- Content -->
+
+    <div class="authentication-wrapper authentication-cover authentication-bg">
+      <div class="authentication-inner row">
+        <!-- /Left Text -->
+        <div class="d-none d-lg-flex col-lg-7 p-0">
+          <div class="auth-cover-bg auth-cover-bg-color d-flex justify-content-center align-items-center">
+            <img
+              src="../../assets/img/illustrations/auth-forgot-password-illustration-light.png"
+              alt="auth-forgot-password-cover"
+              class="img-fluid my-5 auth-illustration"
+              data-app-light-img="illustrations/auth-forgot-password-illustration-light.png"
+              data-app-dark-img="illustrations/auth-forgot-password-illustration-dark.png"
+            />
+
+            <img
+              src="../../assets/img/illustrations/bg-shape-image-light.png"
+              alt="auth-forgot-password-cover"
+              class="platform-bg"
+              data-app-light-img="illustrations/bg-shape-image-light.png"
+              data-app-dark-img="illustrations/bg-shape-image-dark.png"
+            />
+          </div>
         </div>
+        <!-- /Left Text -->
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
+        <!-- Forgot Password -->
+        <div class="d-flex col-12 col-lg-5 align-items-center p-sm-5 p-4">
+          <div class="w-px-400 mx-auto">
+            <!-- Logo -->
+            <div class="app-brand mb-4">
+              <a href="index.html" class="app-brand-link gap-2">
+                <span class="app-brand-logo demo">
+                  <svg width="32" height="22" viewBox="0 0 32 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M0.00172773 0V6.85398C0.00172773 6.85398 -0.133178 9.01207 1.98092 10.8388L13.6912 21.9964L19.7809 21.9181L18.8042 9.88248L16.4951 7.17289L9.23799 0H0.00172773Z"
+                      fill="#7367F0"
+                    />
+                    <path
+                      opacity="0.06"
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M7.69824 16.4364L12.5199 3.23696L16.5541 7.25596L7.69824 16.4364Z"
+                      fill="#161616"
+                    />
+                    <path
+                      opacity="0.06"
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M8.07751 15.9175L13.9419 4.63989L16.5849 7.28475L8.07751 15.9175Z"
+                      fill="#161616"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M7.77295 16.3566L23.6563 0H32V6.88383C32 6.88383 31.8262 9.17836 30.6591 10.4057L19.7824 22H13.6938L7.77295 16.3566Z"
+                      fill="#7367F0"
+                    />
+                  </svg>
+                </span>
+              </a>
             </div>
-        @endif
-
-        <x-validation-errors class="mb-4" />
-
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
-
-            <div class="block">
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <!-- /Logo -->
+            <h3 class="mb-1 fw-bold">Forgot Password? 🔒</h3>
+            <p class="mb-4">Enter your email and we'll send you instructions to reset your password</p>
+            <form id="formAuthentication" class="mb-3" action="auth-reset-password-cover.html" method="POST">
+              <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="email"
+                  name="email"
+                  placeholder="Enter your email"
+                  autofocus
+                />
+              </div>
+              <button class="btn btn-primary d-grid w-100">Send Reset Link</button>
+            </form>
+            <div class="text-center">
+              <a href="{{ route('login') }}" class="d-flex align-items-center justify-content-center">
+                <i class="ti ti-chevron-left scaleX-n1-rtl"></i>
+                Back to login
+              </a>
             </div>
+          </div>
+        </div>
+        <!-- /Forgot Password -->
+      </div>
+    </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-button>
-            </div>
-        </form>
-    </x-authentication-card>
-</x-guest-layout>
+    <!-- / Content -->
+
+    @include('includes.backend.script')
+  </body>
+</html>
