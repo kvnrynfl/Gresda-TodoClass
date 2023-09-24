b143f139bbe994f3d8f40cf1e5617782ca0b411d
Kim Lanisa <lanisriagustin137@gmail.com>
2023-09-24 21:18:29 +0700

    redesign register pages 🔓

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,60 +1,176 @@
-<x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+<!DOCTYPE html>
 
-        <x-validation-errors class="mb-4" />
+<html
+  lang="en"
+  class="light-style customizer-hide"
+  dir="ltr"
+  data-theme="theme-default"
+  data-assets-path="../../assets/"
+  data-template="vertical-menu-template"
+>
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
+    />
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+    <title>Register | To-Do Class</title>
 
-            <div>
-                <x-label for="name" value="{{ __('Name') }}" />
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+    <meta name="description" content="To-Do Class is a simple app to manage your daily tasks with a simple user interface." />
+    <meta name="keywords" content="To-Do Class, To-Do, To-Do List, To-Do App, To-Do List App, To-Do List Application" />
+    <meta name="author" content="Kim & Rey" />
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="../../assets/img/favicon/favicon.ico" />
+    @include('includes.backend.style')
+
+  </head>
+
+  <body>
+    <!-- Content -->
+
+    <div class="authentication-wrapper authentication-cover authentication-bg">
+      <div class="authentication-inner row">
+        <!-- /Left Text -->
+        <div class="d-none d-lg-flex col-lg-7 p-0">
+          <div class="auth-cover-bg auth-cover-bg-color d-flex justify-content-center align-items-center">
+            <img
+              src="../../assets/img/illustrations/auth-register-illustration-light.png"
+              alt="auth-register-cover"
+              class="img-fluid my-5 auth-illustration"
+              data-app-light-img="illustrations/auth-register-illustration-light.png"
+              data-app-dark-img="illustrations/auth-register-illustration-dark.png"
+            />
+
+            <img
+              src="../../assets/img/illustrations/bg-shape-image-light.png"
+              alt="auth-register-cover"
+              class="platform-bg"
+              data-app-light-img="illustrations/bg-shape-image-light.png"
+              data-app-dark-img="illustrations/bg-shape-image-dark.png"
+            />
+          </div>
+        </div>
+        <!-- /Left Text -->
+
+        <!-- Register -->
+        <div class="d-flex col-12 col-lg-5 align-items-center p-sm-5 p-4">
+          <div class="w-px-400 mx-auto">
+            <!-- Logo -->
+            <div class="app-brand mb-4">
+              <a href="index.html" class="app-brand-link gap-2">
+                <span class="app-brand-logo demo">
+                  <svg width="32" height="22" viewBox="0 0 32 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M0.00172773 0V6.85398C0.00172773 6.85398 -0.133178 9.01207 1.98092 10.8388L13.6912 21.9964L19.7809 21.9181L18.8042 9.88248L16.4951 7.17289L9.23799 0H0.00172773Z"
+                      fill="#7367F0"
+                    />
+                    <path
+                      opacity="0.06"
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M7.69824 16.4364L12.5199 3.23696L16.5541 7.25596L7.69824 16.4364Z"
+                      fill="#161616"
+                    />
+                    <path
+                      opacity="0.06"
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M8.07751 15.9175L13.9419 4.63989L16.5849 7.28475L8.07751 15.9175Z"
+                      fill="#161616"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M7.77295 16.3566L23.6563 0H32V6.88383C32 6.88383 31.8262 9.17836 30.6591 10.4057L19.7824 22H13.6938L7.77295 16.3566Z"
+                      fill="#7367F0"
+                    />
+                  </svg>
+                </span>
+              </a>
             </div>
+            <!-- /Logo -->
+            <h3 class="mb-1 fw-bold">Adventure starts here 🚀</h3>
+            <p class="mb-4">Make your app management easy and fun!</p>
 
-            <div class="mt-4">
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            </div>
-
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
-
-            <div class="mt-4">
-                <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
-
-            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
-                <div class="mt-4">
-                    <x-label for="terms">
-                        <div class="flex items-center">
-                            <x-checkbox name="terms" id="terms" required />
-
-                            <div class="ml-2">
-                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
-                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Terms of Service').'</a>',
-                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Privacy Policy').'</a>',
-                                ]) !!}
-                            </div>
-                        </div>
-                    </x-label>
+            <form id="formAuthentication" class="mb-3" action="index.html" method="POST">
+              <div class="mb-3">
+                <label for="username" class="form-label">Username</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="username"
+                  name="username"
+                  placeholder="Enter your username"
+                  autofocus
+                />
+              </div>
+              <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="text" class="form-control" id="email" name="email" placeholder="Enter your email" />
+              </div>
+              <div class="mb-3 form-password-toggle">
+                <label class="form-label" for="password">Password</label>
+                <div class="input-group input-group-merge">
+                  <input
+                    type="password"
+                    id="password"
+                    class="form-control"
+                    name="password"
+                    placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                    aria-describedby="password"
+                  />
+                  <span class="input-group-text cursor-pointer"><i class="ti ti-eye-off"></i></span>
                 </div>
-            @endif
+              </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
+              <div class="mb-3">
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" id="terms-conditions" name="terms" />
+                  <label class="form-check-label" for="terms-conditions">
+                    I agree to
+                    <a href="javascript:void(0);">privacy policy & terms</a>
+                  </label>
+                </div>
+              </div>
+              <button class="btn btn-primary d-grid w-100">Sign up</button>
+            </form>
 
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
+            <p class="text-center">
+              <span>Already have an account?</span>
+              <a href="{{ route('login') }}">
+                <span>Sign in instead</span>
+              </a>
+            </p>
+
+            <div class="divider my-4">
+              <div class="divider-text">or</div>
             </div>
-        </form>
-    </x-authentication-card>
-</x-guest-layout>
+
+            <div class="d-flex justify-content-center">
+              <a href="javascript:;" class="btn btn-icon btn-label-facebook me-3">
+                <i class="tf-icons fa-brands fa-facebook-f fs-5"></i>
+              </a>
+
+              <a href="javascript:;" class="btn btn-icon btn-label-google-plus me-3">
+                <i class="tf-icons fa-brands fa-google fs-5"></i>
+              </a>
+
+              <a href="javascript:;" class="btn btn-icon btn-label-twitter">
+                <i class="tf-icons fa-brands fa-twitter fs-5"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+        <!-- /Register -->
+      </div>
+    </div>
+
+    <!-- /Content -->
+
+    @include('includes.backend.script')
+  </body>
+</html>
