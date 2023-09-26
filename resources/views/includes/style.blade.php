b8f1817a7853f385494c07db5e2e412f6ba99fa7
Kevin Reynaufal <kevinreynaufal2004@gmail.com>
2023-09-26 13:01:55 +0700

    Add pavicon to include style

--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -1,3 +1,6 @@
+<!-- Favicon -->
+<link rel="icon" type="image/x-icon" href="../../assets/img/favicon/favicon.ico" />
+
 <!-- Fonts -->
 <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
